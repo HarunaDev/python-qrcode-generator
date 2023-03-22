2de8062ace630235571b5d5a0d684826e64dc9dd
Alvin Haruna <harunaalvin69@gmail.com>
2023-03-22 16:16:23 +0100

    imported qrcode library & created function to generate qr code

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 # install librarires needed
+import qrcode
 
 # create function that receives text and converts it to qr-code
-
+def qr_generator(text):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border = 4
+    )
 # save qr-code as image
 
 # run function
